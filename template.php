244972e84159ae2c0ff48ecac43575f9b8ee2172
Ellen Gummesson <ellengummesson@gmail.com>
2012-12-30 15:50:08 +0100

    Small indentation update.

--- template.php
+++ template.php
@@ -1,9 +1,9 @@
 <?php include('header.inc.php'); ?>
 
-<?php include('nav.inc.php'); ?>
+  <?php include('nav.inc.php'); ?>
 
-<?php include('sidebar.inc.php'); ?>
+    <?php include('sidebar.inc.php'); ?>
 
-<?php include('page.inc.php'); ?>
+  <?php include('page.inc.php'); ?>
 
 <?php include('footer.inc.php'); ?>
